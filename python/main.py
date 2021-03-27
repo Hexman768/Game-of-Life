582a8024e9ba2646bce5c7b9502dee05f97468c7
Hexman768 <Zachary.Pedigo1@gmail.com>
2021-03-27 00:05:12 -0500

    Added function to print board

--- python/main.py
+++ python/main.py
@@ -1,8 +1,13 @@
 import os
 from random import randrange
 
-
 def create_board(rows, cols):
+    """Creates and returns a grid based on the given number of rows and columns.
+
+    Args:
+    rows -- Number of rows for the grid
+    cols -- Number of columns for the grid
+    """
     grid = []
     for i in range(rows):
         row = []
@@ -11,9 +16,16 @@ def create_board(rows, cols):
             row.append(cell)
         grid.append(row)
     return grid
+
+def print_board(board, cols):
+    for i in range(len(board)):
+        for j in range(cols):
+            print(" {} ".format(board[i][j]), end=" ")
+        print("\n")
     
 def main():
-    print(create_board(10, 10))
+    board = create_board(10, 10)
+    print_board(board, 10)
 
 if __name__ == "__main__":
     main()
